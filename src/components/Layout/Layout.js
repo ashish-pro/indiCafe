6e6c58e32b27f3469a000a56427d43518aebe846
ashish-pro <ashishbit017@outlook.com>
2023-11-04 19:52:14 +0530

    layout-design

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Layout.css'
+import Navbar from './Navbar/Navbar'
+import Main from './Main/Main'
+import  Footer  from './Footer/Footer'
+
+const Layout = () => {
+    
+  return (
+    <>
+    <Navbar />
+    <Main />
+    <Footer />
+    </>
+  )
+}
+
+export default Layout;
