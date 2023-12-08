d8e5e73e76ec1dec24a12758c3132bfc7cd52261
ashish-pro <ashishbit017@outlook.com>
2023-12-08 23:36:33 +0530

    landing page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Layout from '../Layout/Layout'
 import './LandingPage.css'
+import Menu from "../LandingPage/Menu/Menu"
 import { Link } from 'react-router-dom'
 
 const LandingPage = () => {
   return (
     <>
-    <Layout title="Serve Our Delicies" background= {true} backdrop = {true}>
-       <section>
+    <Layout title="Swadisht Bharat ka indiCafe: Ghar ka Swaad!" background= {true} backdrop = {true}>
+       <section className='l-section'>
         <h1 className='l-title'>indi<span className='l-title-span'>Caf<span>&eacute;</span></span></h1>
         <p>
           <button className='btn btn-warning l-btn'>
@@ -17,6 +18,7 @@ const LandingPage = () => {
         <p>
           Home Delivery in 30 min flat.
         </p>
+        <Menu/>
         </section> 
     </Layout>
     </>
